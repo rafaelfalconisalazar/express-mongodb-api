5f891979c31b7b63bf5734d1ee9438cb2d22a53e
rafael <rfalconi@unibe.edu.ec>
2020-06-15 15:06:07 -0500

    add socket.io

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const app = express();
 const routes = require('./routers/api');
 const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
+const http= require('http');
 mongoose.connect('mongodb+srv://Falconi:edu900fs@cluster0-iput3.mongodb.net/test?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
 app.use(bodyParse.json());
 app.use(routes);
 
-
-app.listen(3000, function () {
-    console.log('ready')
+var server=http.Server(app);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log(`Listening on ${PORT}`)
 });
-
-
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+});
