7e2c8a80d6f83ce9d0bd820c71e6b6219bb4c075
rafael <rfalconi@unibe.edu.ec>
2020-06-15 15:20:27 -0500

    ee

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ mongoose.Promise = global.Promise;
 app.use(bodyParse.json());
 app.use(categoryRouter);
 app.use(productRouter);
-var server = http.Server(app)
+var server = http.Server(app);
 const PORT = process.env.PORT || 3000;
-app.listen(3000, function () {
-    console.log(`Listening on ${PORT}`);
-
+app.listen(PORT, function () {
+    console.log(`Listening on ${PORT}`)
 });
-
-
-var io=require('socket.io')(server);
+const io = require('socket.io')(server);
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
