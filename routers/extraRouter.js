7aafcd5ce818d24d09ef7155987f18de6cc265f2
rafael <r.falconi9030@gmail.com>
2020-11-26 08:54:37 -0500

    add new route

--- routers/extraRouter.js
+++ routers/extraRouter.js
@@ -7,4 +7,9 @@ router.get(uri, function (req, res) {
     res.send("hola mundo");
 })
 
+router.get(uri+"/:id", function (req, res) {
+    let id = req.params.id;
+    res.send("hola: "+idø);
+})
+
 module.exports = router;
